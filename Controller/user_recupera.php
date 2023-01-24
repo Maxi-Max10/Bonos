a1e898c63728026fbead36392e90b3ab5165a0cd
Abel Tapíz Sosa <abeltapiz@hotmail.com>
2023-01-24 18:46:51 -0300

    Agregado clave aplicación

--- Controller/user_recupera.php
+++ Controller/user_recupera.php
@@ -31,7 +31,7 @@ if ($nr == 1 ) {
 		$mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
 		$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
 		$mail->Username   = 'gasopensas@gmail.com';                     //SMTP username
-		$mail->Password   = 'J3Y97vp64tLR6YA';                               //SMTP password
+		$mail->Password   = 'trpepakperulluqu';                               //SMTP password
 		$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
 		$mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
