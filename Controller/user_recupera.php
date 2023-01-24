97537812f3d8267796c7fcca50e3b1b1c797b611
Abel Tapíz Sosa <abeltapiz@hotmail.com>
2023-01-24 16:44:07 -0300

    Modificación recupera contraseña a correo

--- Controller/user_recupera.php
+++ Controller/user_recupera.php
@@ -1,3 +1,34 @@
 <?php
-header('Location: ../loginUS.php?alerta=si'); 
+include '../Model/conexion_usuario.php';
+
+$correo = $_POST['txtcorreo'];
+$cuil = $_POST['cuil'];
+
+
+$consulta = ("SELECT * FROM personal WHERE cuil = '$cuil'");
+$queryusuario 	= mysqli_query($conexionUs ,$consulta);
+$nr 			= mysqli_num_rows($queryusuario); 
+if ($nr > 0)
+{
+	$mostrar		= mysqli_fetch_array($queryusuario); 
+	$enviarpass 	= $mostrar['password_personal'];
+
+	$paracorreo 		= $correo;
+	$titulo				= "Recuperar contraseña";
+	$mensaje			= $enviarpass;
+	$tucorreo			= "From: abelisho.sabe@gmail.com";
+
+	if(mail($paracorreo,$titulo,$mensaje,$tucorreo))
+	{
+		echo "<script> alert('Contraseña enviada');window.location= '../loginUs.php' </script>";
+	}else
+	{
+		echo "<script> alert('Error');window.location= '../loginUs.php' </script>";
+	}
+}
+else
+{
+	echo "<script> alert('Este usuario no existe');window.location= '../loginUs.php' </script>";
+}
+/*VaidrollTeam*/
 ?>
