e8ca25508a0cb239cfbc07d7c9c0d8c7ad57c523
Maxi-Max10 <maxgiu3@gmail.com>
2023-01-26 17:11:14 -0300

    doc

--- admin/index_bonos.php
+++ admin/index_bonos.php
@@ -27,8 +27,7 @@ if (!isset($_SESSION['idadmin'])) {
     <title>Gasopen S.A.S | Administración</title>
 
     <!-- Bootstrap CSS v5.2.1 -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link href="Includes/css/bootstrap.min.css" rel="stylesheet">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
     <!--<link rel="stylesheet" href="Includes/css/style.css">-->
@@ -400,13 +399,8 @@ if (!isset($_SESSION['idadmin'])) {
     </footer>
 
     <!-- Bootstrap JavaScript Libraries -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
-        integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
-    </script>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
-        integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
-    </script>
+    <script src="Includes/js/popper.min.js"></script>
+    <script src="Includes/js/bootstrap.min.js"></script>
 
     <script src="Includes/js/jquery-3.6.3.min.js"></script>
     <!--<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>-->
--- admin/login.php
+++ admin/login.php
@@ -16,8 +16,9 @@ if (isset($_SESSION['idadmin'])) {
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <!-- Bootstrap CSS v5.2.1 -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+  <link href="Includes/css/bootstrap.min.css" rel="stylesheet">
+  
+  <!-- Favicon -->
    <link href="Includes/assets/gasIcon.ico" rel="shortcut icon" type="image/x-icon">
 </head>
 
@@ -70,13 +71,8 @@ if (isset($_SESSION['idadmin'])) {
 
 
   <!-- Bootstrap JavaScript Libraries -->
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
-    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
-  </script>
+  <script src="Includes/js/popper.min.js"></script>
+  <script src="Includes/js/bootstrap.min.js"></script>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
-    integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
-  </script>
 </body>
-
 </html>
