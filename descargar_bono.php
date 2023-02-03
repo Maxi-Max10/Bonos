dc0b2bdd4d1053a6ba571d795a9d64908fc69809
Abel Tapíz Sosa <abeltapiz@hotmail.com>
2023-02-03 17:45:35 -0300

    Modificación de index usuario y arreglo descargar_bono

--- descargar_bono.php
+++ descargar_bono.php
@@ -2,18 +2,25 @@
 include("Model/conexion_usuario.php");
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
-    $query = "SELECT * FROM bonos WHERE personal_idpersonal = $id";
+    $idBono = $_GET['bn'];
+  
+    $query = "SELECT * FROM bonos WHERE personal_idpersonal =".$id." AND id_bonos=".$idBono;
     $result = mysqli_query($conexionUs, $query);
     if (mysqli_num_rows($result) == 1){
         $row = mysqli_fetch_array($result);
-        $enlace = 'admin/Controller/files/'.$row['bonos'];
+        $enlace = "admin/Controller/files/".$row['bonos'];
         $basefichero = basename($enlace);
+
+        header("Content-Type: application/force-download");
      
         header( "Content-Type: application/octet-stream");
-     
+
+        header("Content-Type: application/download");
+
+    
         header( "Content-Length: ".filesize($enlace));
      
-        header( "Content-Disposition:attachment;filename=" .$basefichero."");
+        header( "Content-Disposition:attachment;filename=" .$basefichero);
         readfile($enlace);
     }
    }
