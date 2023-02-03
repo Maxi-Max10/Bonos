cd04f33ba3fd72649232d15055404192d6af7358
Maxi-Max10 <maxgiu3@gmail.com>
2023-02-03 15:42:18 -0300

    table

--- admin/Includes/js/main.js
+++ admin/Includes/js/main.js
@@ -2,12 +2,14 @@
 $(document).ready(function() {
     $('#tabla_personal').DataTable( {
         columnDefs: [
+            { orderable: false, targets: 3 },
             { orderable: false, targets: 4 },
             { orderable: false, targets: 5 },
             { orderable: false, targets: 6 },
             { orderable: false, targets: 7 },
             { orderable: false, targets: 8 },
           ],
+        destroy: true,
         language: {
             url: 'https://cdn.datatables.net/plug-ins/1.13.2/i18n/es-ES.json'
         }
